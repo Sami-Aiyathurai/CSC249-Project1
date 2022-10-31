83643a23b51360495ee7c96f5a98cfb0e4406505
Megan Varnum <mvarnum@smith.edu>
2022-10-31 19:53:31 -0400

    (in progress) phase 2 & 3

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+print(sys.argv)
+server_host = sys.argv[1]
+server_port = int(sys.argv[2])
+filename = sys.argv[3]
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((filename, server_port))
+
+request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % filename
+client.send(request.encode())
+response = client.recv(1024)
+print(response.decode())
--- webserver.py
+++ webserver.py
@@ -1,5 +1,33 @@
 from socket import *
 import sys # In order to terminate the program
+from threading import *
+
+class MyThread(Thread):
+    def __init__(address, socket, self):
+        Thread.__init__(self)
+        self.connectionSocket = socket
+        #self.addr = address
+        print("__init__()")
+
+    def run(self):
+        print("run()")
+        message = ""
+        while True:
+            try:
+                message = self.connectionSocket.recv(1024)
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata = f.read()
+                self.connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+                for i in range(0, len(outputdata)):
+                    self.connectionSocket.send(outputdata[i].encode())
+                self.connectionSocket.send("\r\n".encode())
+                self.connectionSocket.close()
+            except IOError:
+                self.connectionSocket.send("HTTP/1.1 404 Not found\r\n\r\n".encode())
+                self.connectionSocket.close()
+
+
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -35,28 +63,30 @@ while True:
     # Fill in end
     # -----------
 
-    try:
+    new_thread = MyThread(addr, connectionSocket)
+    new_thread.start()
+    #try:
         
         # -------------
         # Fill in start
         # -------------
-        message = connectionSocket.recv(1024) # TODO: Receive the request message from the client
+        #message = connectionSocket.recv(1024) # TODO: Receive the request message from the client
         # -----------
         # Fill in end
         # -----------
         
         # Extract the path of the requested object from the message
 		# The path is the second part of HTTP header, identified by [1]
-        filename = message.split()[1]
+        #filename = message.split()[1]
 
         # Because the extracted path of the HTTP request includes 
 		# a character '\', we read the path from the second character
-        f = open(filename[1:])
+        #f = open(filename[1:])
         
         # -------------
         # Fill in start
         # -------------
-        outputdata = f.read() # TODO: Store the entire contents of the requested file in a temporary buffer
+        #outputdata = f.read() # TODO: Store the entire contents of the requested file in a temporary buffer
         # -----------
         # Fill in end
         # -----------
@@ -65,26 +95,26 @@ while True:
         # Fill in start
         # -------------
         # TODO: Send one HTTP header line into socket
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        #connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         # -----------
         # Fill in end
         # -----------
 
         # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        # for i in range(0, len(outputdata)):
+        #     connectionSocket.send(outputdata[i].encode())
+        # connectionSocket.send("\r\n".encode())
 
-        connectionSocket.close()
+        # connectionSocket.close()
 
-    except IOError:
+    #except IOError:
         # -------------
         # Fill in start
         # -------------
             # TODO: Send response message for file not found
             #       Close client socket
-        connectionSocket.send("HTTP/1.1 404 Not found\r\n\r\n".encode())
-        connectionSocket.close()
+        #connectionSocket.send("HTTP/1.1 404 Not found\r\n\r\n".encode())
+        #connectionSocket.close()
         # -----------
         # Fill in end
         # -----------
